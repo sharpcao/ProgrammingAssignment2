ae69ddcb622d1ff8064d9380d3d97ca97ce3dc84
Sharpcao <674106780@qq.com>
2014-06-11 11:28:07 +0800

    Signed-off-by: Sharpcao <674106780@qq.com>

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(xm = matrix()) {
+	ixm <- NULL;
+	set <- function(x){
+		xm <<- x;
+		ixm <<- NULL;
+	}
+	get <- function() xm;
+	
+	setInv <- function(inv) ixm <<- inv;
+	getInv <- function() ixm;
+	
+	list(set=set,get=get,setInv=setInv,getInv=getInv);
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cm, ...) {
+        
+	ixm <- cm$getInv();
+	if(!is.null(ixm)){
+		message("getting cached data");
+		return(ixm);
+	}
+	ixm <- solve(cm$get(),...);
+	cm$setInv(ixm);
+	## Return a matrix that is the inverse of 'x'
+	ixm;
 }
